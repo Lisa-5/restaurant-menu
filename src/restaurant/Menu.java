f023785f60830f23ae635accb9ade2ec8045a2d3
Lisa Ulricksen <LisaHome@lisas-imac.attlocal.net>
2022-03-28 19:38:18 -0500

    Studio Chapter 4 finished

--- src/restaurant/Menu.java
+++ src/restaurant/Menu.java
@@ -1,4 +1,19 @@
 package restaurant;
 
+import java.util.ArrayList;
+
 public class Menu {
+
+    public static void main(String[] args) {
+
+        ArrayList<MenuItem> menu = new ArrayList();
+        menu.add(new MenuItem("Mozzarella Sticks", 9.00, "fried cheese", "appetizer", true));
+        menu.add(new MenuItem("Mac & Cheese", 7.99, "pasta and cheesy sauce", "main course", true));
+        menu.add(new MenuItem("Cheesecake", 12.50, "Cream Cheese delight", "dessert", true));
+
+    }
+
+
+
+
 }
--- src/restaurant/MenuItem.java
+++ src/restaurant/MenuItem.java
@@ -1,4 +1,71 @@
 package restaurant;
 
+import java.util.Date;
+
 public class MenuItem {
+
+    private String itemName;
+    private double price;
+    private String description;
+    private String category;
+    private boolean newItem;
+    private Date createdAt;
+
+    public MenuItem(String itemName, double price, String description, String category, boolean newItem){
+        this.itemName = itemName;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.newItem = newItem;
+        this.createdAt = new Date();
+    }
+
+    public String getItemName(){
+        return itemName;
+    }
+
+    public void setItemName(String aItemName) {
+        itemName = aItemName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double aPrice) {
+        this.price = aPrice;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String aDescription) {
+        this.description = aDescription;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String aCategory) {
+        this.category = aCategory;
+    }
+
+    public boolean isNewItem() {
+        return newItem;
+    }
+
+    public void setNewItem(boolean aNewItem) {
+        this.newItem = aNewItem;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date aCreatedAt) {
+        this.createdAt = aCreatedAt;
+    }
+
 }
